2e4a6829f3dd5f3ceaecb6070b1790b5ef9d1545
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2022-03-18 10:42:48 -1000

    complete deletion and replacmenet im freaking out IT"S BROKEN

--- src/pythagoreanTheorem.js
+++
@@ -1,8 +0,0 @@
-const pythagoreanTheorem = (checkNode, endNode) => {
-  const a = Math.abs(checkNode.i - endNode.i);
-  const b = Math.abs(checkNode.j - endNode.j);
-  const c = Math.sqrt((a * a) + (b * b));
-  return c;
-}
-
-export default pythagoreanTheorem;
